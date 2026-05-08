8e52d7ce2628023a3140e7b0d56070de44a2e705
Brian Crescimanno <brian.crescimanno@me.com>
2026-05-08 03:24:02 -0400

    chore(deps): update ghcr.io/qdm12/gluetun:latest docker digest to f2f2cc3 (#193)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- modules/arr-stack.nix
+++ modules/arr-stack.nix
@@ -52,7 +52,7 @@ in
   virtualisation.oci-containers.containers = {
 
     gluetun = {
-      image = "ghcr.io/qdm12/gluetun:latest@sha256:0d60fc67b2847ffb9b3ec2b42a4f5523c38e0e464974e71a640e8dcf0a52a0ee";
+      image = "ghcr.io/qdm12/gluetun:latest@sha256:f2f2cc3643e4ab136ea8f0422cfccc7f2064465dd8301572b83f5fb80877e085";
       autoStart = true;
       volumes = [
         "/var/lib/gluetun:/gluetun"
