7746f13d6a89aa60c2420bad63882276dcabb613
Brian Crescimanno <brian.crescimanno@me.com>
2026-05-08 15:11:40 -0400

    chore(deps): update ghcr.io/qdm12/gluetun:latest docker digest to 047631a (#195)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- modules/arr-stack.nix
+++ modules/arr-stack.nix
@@ -52,7 +52,7 @@ in
   virtualisation.oci-containers.containers = {
 
     gluetun = {
-      image = "ghcr.io/qdm12/gluetun:latest@sha256:f2f2cc3643e4ab136ea8f0422cfccc7f2064465dd8301572b83f5fb80877e085";
+      image = "ghcr.io/qdm12/gluetun:latest@sha256:047631a09616eafcf790ccb504ae3b78aed58274c1ef84593a12567f67a4484c";
       autoStart = true;
       volumes = [
         "/var/lib/gluetun:/gluetun"
