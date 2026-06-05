0f383d868a8cdc55884b8b08d5a2c274595665d6
Brian Crescimanno <brian.crescimanno@me.com>
2026-06-05 02:38:52 -0700

    chore(deps): update openthread/otbr:latest docker digest to e91620a (#304)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- modules/homeassistant.nix
+++ modules/homeassistant.nix
@@ -88,7 +88,7 @@
     #   With those modules loaded, iptables-legacy inside the container can
     #   coexist with the host's nftables firewall.
     otbr = {
-      image = "openthread/otbr:latest@sha256:9c365f9cf8e942ddc5c96afd74fb8ba549f1c9c7b6fb4c4be645e882b212d881";
+      image = "openthread/otbr:latest@sha256:e91620a133100c7852251b509853d1224d549ad5129c0c325e75aae25c6cf922";
       autoStart = true;
       cmd = [
         "--radio-url" "spinel+hdlc+uart:///dev/ttyACM0?uart-baudrate=460800"
