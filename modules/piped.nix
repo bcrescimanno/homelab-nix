5e51dfae4f5d2a2165235130181d9d6d5fb2138a
Brian Crescimanno <brian.crescimanno@me.com>
2026-08-10 00:20:44 -0700

    chore(deps): update docker.io/1337kavin/piped-proxy:latest docker digest to 0bd23e5 (#567)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- modules/piped.nix
+++ modules/piped.nix
@@ -165,7 +165,7 @@ in
     };
 
     piped-proxy = {
-      image = "docker.io/1337kavin/piped-proxy:latest@sha256:e25d481b1ea375ab61a8f070ca3809842ffeeada9606275a110d088cb57e13a8";
+      image = "docker.io/1337kavin/piped-proxy:latest@sha256:0bd23e5c86b804ec10e6dcbee22b89af848b2d4e1d45fed5464adfcff4019847";
       autoStart = true;
       extraOptions = [ "--network=piped" ];
       ports = [ "8182:8080" ];
